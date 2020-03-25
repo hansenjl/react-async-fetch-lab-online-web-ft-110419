fac67008703ab6e62e179f27ac4912d7fc242053
hansenjl <jenn.leigh.hansen@gmail.com>
2020-03-24 20:43:08 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+       people: []
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+            .then(res => res.json())
+            .then(astros => this.setState({people: astros.people}))
+    }
+
+    renderAstros(){
+        if(this.state.people !== []){
+            return(
+            <ul>
+                {this.state.people.map( a => <li>{a.name} - {a.craft}</li>)}
+            </ul>
+            )
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                {this.renderAstros()}
+            </div>
+        )
+    }
+}
+
+export default App;
